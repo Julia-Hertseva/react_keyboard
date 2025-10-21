7468d7e12c96322270fd0094a4afe784ae6070a9
Julia-Hertseva <gerceva@gmail.com>
2025-10-21 13:39:43 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export const App: React.FC = () => {
-  const [pressedKey, setPressedKey] = useState<string>('');
-
-  useEffect(() => {
-    const handleKeyPress = (event: KeyboardEvent): void => {
-      setPressedKey(event.key);
-    };
-
-    document.addEventListener('keyup', handleKeyPress);
-
-    return () => {
-      document.removeEventListener('keyup', handleKeyPress);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <p className="App__message">
-        {pressedKey
-          ? `The last pressed key is [${pressedKey}]`
-          : 'Nothing was pressed yet'}
-      </p>
-    </div>
-  );
+type State = {
+  pressedKey: string | null;
 };
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: null,
+  };
+
+  handleKeyPress = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyPress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
